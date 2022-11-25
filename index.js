34bc12fadc4fc7bb2c142663501031c4758bfa83
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-11-25 11:51:39 +0600

    server site setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+require ('dotenv').config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get ('/', async( req, res ) => {
+    res.send('server side is running');
+});
+app.listen(port, () => console.log(`server running on ${port}`));
