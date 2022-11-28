e52766c50ea9ce18c968565c117b7e427d753145
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-11-28 19:01:03 +0600

    admin

--- index.js
+++ index.js
@@ -50,13 +50,32 @@ async function run(){
             res.send(result);
         });
 
+        app.get('/users', async(req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+
         // users
         app.post('/users', async(req, res) => {
             const user = req.body;
             console.log(user);
             const result = await usersCollection.insertOne(user);
             res.send(result);
-        })
+        });
+        // admin
+        app.put('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
 
     }
     finally{
